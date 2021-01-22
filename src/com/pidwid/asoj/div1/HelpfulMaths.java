1dafd4d243ad8000c9e0ac2a163562ed0ebbc975
pidwid <luckyshukla765@gmail.com>
2021-01-22 12:23:39 +0530

    22/jan/21

--- src/com/pidwid/asoj/div1/HelpfulMaths.java
+++ src/com/pidwid/asoj/div1/HelpfulMaths.java
@@ -1,5 +1,6 @@
 package com.pidwid.asoj.div1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 // 339A - Helpful Maths
@@ -8,6 +9,19 @@ public class HelpfulMaths {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String text = sc.nextLine();
-
+    String[] arr = text.split("\\+");
+    Integer[] intarray=new Integer[arr.length];
+    int i=0;
+    for(String str:arr){
+      intarray[i]=Integer.parseInt(str.trim());
+      i++;
+    }
+    Arrays.sort(intarray);
+    StringBuilder res = new StringBuilder();
+    for(int j = 0; j < intarray.length; j++){
+      if(j == (intarray.length -1)) res.append(intarray[j]);
+      else res.append(intarray[j]).append('+');
+    }
+    System.out.println(res);
   }
 }
--- src/com/pidwid/asoj/div1/Twins.java
+++ src/com/pidwid/asoj/div1/Twins.java
@@ -1,5 +1,7 @@
 package com.pidwid.asoj.div1;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 // 160A - Twins
@@ -7,5 +9,24 @@ public class Twins {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int numOfCoins = sc.nextInt();
+    Integer[] coinArray = new Integer[numOfCoins];
+    int coinSum = 0;
+    for(int i = 0; i < numOfCoins; i++){
+      coinArray[i] = sc.nextInt();
+      coinSum += coinArray[i];
+    }
+    int mid = coinSum/2;
+    Arrays.sort(coinArray, Collections.reverseOrder());
+    int moreThanMid = 0;
+    int count  = 0;
+    for (Integer coin : coinArray) {
+      if (moreThanMid <= mid) {
+        moreThanMid += coin;
+        count++;
+      }
+    }
+    System.out.println(count);
+    sc.close();
   }
 }
